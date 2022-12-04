4e3b08e3a1dd8ce9802ff0cf1afaa29c3cd39bf6
NITRO-AN515-56\Nam <namyahoo2003@gmail.com>
2022-12-04 23:04:34 +0700

    first commit

---
+++ chuHoa/script.js
@@ -0,0 +1,12 @@
+function isFirstLetterUpperCase(str) {
+    console.log(str);
+    const regexp = /^[A-Z]/;
+    if (regexp.test(str)) {
+        console.log("String's first character is uppercase");
+    } else {
+        console.log("String's first character is not uppercase");
+    }
+}
+
+isFirstLetterUpperCase("Abcd");
+isFirstLetterUpperCase("abcd");
